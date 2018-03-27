a6e50af1a711850d496e08145d4d9572770f684e
kkennelly <kkennelly@ycp.edu>
2018-03-27 17:05:38 -0400

    Optimize the code in DerbyDatabase class

--- src/persist/DerbyDatabase.java
+++ src/persist/DerbyDatabase.java
@@ -1,44 +1,20 @@
 package persist;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.List;
 
-import model.Profile;
-import model.PhysicalModel;
-
-public class DerbyDatabase {
-	
-	//Is now implemented only to work with a fake database
-	//needs to be changed in the future to incorporate the real derby database
-	public void loadInitialData() throws SQLException {
-		List<Profile> profileList;
-		List<PhysicalModel> physicalModelList;
-		
-		try {
-			profileList = InitialData.getProfiles();
-			//physicalModelList = InitialData.getPhysicalModels();
-		} catch (IOException e) {
-			throw new SQLException("Couldn't read initial data", e);
-		}
-	}
-	
+public class DerbyDatabase {	
 	
 	// The main method creates the database tables and loads the initial data.
 	public static void main(String[] args) throws IOException {
 		System.out.println("Creating tables...");
-		DerbyDatabase db = new DerbyDatabase();
+		//DerbyDatabase db = new DerbyDatabase();
+		
 		//db.createTables();
 			
-		System.out.println("Loading initial data...");
-		try {
-			db.loadInitialData();
-		} catch(SQLException e) {
-			System.out.println(e.getMessage());
-		}
-			
+		System.out.println("Loading fake database initial data...");
+	
+		FakeDatabase fdb = new FakeDatabase();
+		
 		System.out.println("Success!");
 	}
 }
