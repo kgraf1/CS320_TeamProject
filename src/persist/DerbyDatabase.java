fc8f39d7e434abc418f0ace6db734891c850ee7b
kkennelly <kkennelly@ycp.edu>
2018-03-30 16:38:17 -0400

    Create DatabaseProvider

--- src/persist/DerbyDatabase.java
+++ src/persist/DerbyDatabase.java
@@ -1,8 +1,16 @@
 package persist;
 
 import java.io.IOException;
+import java.util.List;
 
-public class DerbyDatabase {	
+import model.Application;
+import model.Category;
+import model.Material;
+import model.PhysicalModel;
+import model.Profile;
+import model.Rating;
+
+public class DerbyDatabase implements IDatabase {	
 	
 	// The main method creates the database tables and loads the initial data.
 	public static void main(String[] args) throws IOException {
@@ -17,4 +25,19 @@ public class DerbyDatabase {
 		
 		System.out.println("Success!");
 	}
+	
+	//finding models
+	public List<PhysicalModel> findModelsByProfileId(Long id) { return null; }
+	public List<PhysicalModel> findModelsByTitle(String title) { return null; }
+	public List<PhysicalModel> findModelsByCategory(Category category) { return null; }
+	public List<PhysicalModel> findModelsByKeyword(String keyword) { return null; }
+	public List<PhysicalModel> findModelsByMaterialName(String materialName) { return null; }
+	
+	//finding users
+	public List<Profile> findProfileByDatabaseTitle(String title) { return null; }
+	
+	//creating models
+	public PhysicalModel insertModel(String title, List<String> procedureList, List<String> keywords, String decription,
+			String thumbnail, String engPrinciple, String citation, Application application, List<Rating> ratings,
+			List<Material> materialList, Category category) { return null; }
 }
