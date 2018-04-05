dfca04527ae2b24824720b22cf4045cdd2c5333f
Jason Constantine <jconstantine@ycp.edu>
2018-04-05 12:41:52 -0400

    Test file for testing controllers added

---
+++ junit/DatabaseTest/ControllersTest.java
@@ -0,0 +1,47 @@
+package DatabaseTest;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import controllers.*;
+import persist.DatabaseProvider;
+import persist.FakeDatabase;
+import persist.IDatabase;
+import model.PhysicalModel;
+import model.Material;
+import model.Profile;
+import model.Keyword;
+
+public class ControllersTest {
+	private IDatabase db = null;
+	
+	@Before
+	public void setUp () throws Exception{
+		//DatabaseProvider.setInstance(new FakeDatabase());
+		//db = DatabaseProvider.getInstance();
+	}
+	
+	@Test
+	public void testAddApplication() {
+		int modelId = 0;
+		String test = "test";
+		
+		AddApplicationController controller = new AddApplicationController();
+		assertTrue(controller.create(modelId, test, test, test, test));
+		assertFalse(controller.create(modelId, test, test, test, test));
+	}
+	
+	@Test
+	public void testAddKeywords() {
+		int modelId = 0;
+		List<String> test;
+		
+		//AddKeywordsController controller = new AddKeywordsController();
+		//assertTrue(controller.create(modelId, test, test, test, test));
+		//assertFalse(controller.create(modelId, test, test, test, test));
+	}
+}
